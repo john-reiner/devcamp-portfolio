a1810b25577f68ae4a2a2ed1a7fe49732971d8fa
john-reiner <johnreiner1990@gmail.com>
2020-12-13 14:25:52 -0500

    Created seeds frile for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,11 @@ end
 end
 
 9.times do |portfolio_item|
-    title: "Portfolio title: #{portfolio_item}", 
-    subtitle: "An awesome subtitle",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://via.placeholder.com/500",
-    thumb_image: "https://via.placeholder.com/150",
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "An awesome subtitle",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "https://via.placeholder.com/500",
+        thumb_image: "https://via.placeholder.com/150"
+    )
 end
