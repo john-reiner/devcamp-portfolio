e22a4c84e0cad658af58f6f1fa44d66c1ef7130d
john-reiner <johnreiner1990@gmail.com>
2020-12-13 15:22:01 -0500

    Integrated new and create actions for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 10.times do |blog|
     Blog.create!(
-        title: "My Blog Post #{blog + 1}",
-        body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+        title: Faker::TvShows::RickAndMorty.character,
+        body: Faker::TvShows::RickAndMorty.quote
     )
 end 
 
 5.times do |skill| 
     Skill.create!(
-        title: "Rails #{skill}",
+        title: Faker::TvShows::MichaelScott.quote,
         percent_utilized: 20
     )
 end
 
 9.times do |portfolio_item|
     Portfolio.create!(
-        title: "Portfolio title: #{portfolio_item}",
-        subtitle: "An awesome subtitle",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        title: Faker::TvShows::SouthPark.character,
+        subtitle: Faker::Movies::Departed.quote,
+        body: Faker::TvShows::SouthPark.quote,
         main_image: "https://via.placeholder.com/500",
         thumb_image: "https://via.placeholder.com/150"
     )
