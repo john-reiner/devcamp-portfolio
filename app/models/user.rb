1a478dc0ed922db423f95ad8f5b7c09bafa7102b
john-reiner <johnreiner1990@gmail.com>
2021-01-06 10:03:41 -0500

    added session tracker for dynamic source links

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
-            validates_presence_of :name
+  validates_presence_of :name
 
     def first_name 
       self.name.split.first 
